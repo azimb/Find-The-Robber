8321fd0aeffd303ac7f5740a28ef69b8a3496333
baghadiya <38919150+baghadiya@users.noreply.github.com>
2018-07-02 22:24:16 -0400

    Create Places.java

---
+++ src/Places.java
@@ -0,0 +1,7 @@
+public abstract class Places{
+	public abstract int timeLost(); 
+	public abstract String getName();
+	public Player hintPlayer;
+	public abstract String getHint();
+	public abstract String getThing();  
+}
